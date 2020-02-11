3944c2036f3595775b6fb3d9395d634edc5037e1
Chimuanya Ibecheozor <chimuanyaibecheozor@gmail.com>
2020-02-11 12:03:50 +0100

    Update

--- djangoapiapp/settings.py
+++ djangoapiapp/settings.py
@@ -164,5 +164,5 @@ LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "login"
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
-
-
+import django_heroku
+django_heroku.settings(locals())
