5e871d13f99c717d820a6b6811a6c1a2126b4098
bruce619 <chimuanyaibecheozor@gmail.com>
2020-09-22 01:54:30 +0100

    ignore

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\CHIMUANYA\Documents\python\djangoapiapp\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
