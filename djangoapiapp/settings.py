1b7728db69fc85f11dacb38ca49d44554ff02e1c
bruce619 <chimuanyaibecheozor@gmail.com>
2020-09-24 00:07:01 +0100

    initial commit

--- djangoapiapp/settings.py
+++ djangoapiapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rest-web-api-vgg.herokuapp.com', 'https://www.rest-web-api-vgg.herokuapp.com']
 
 
 # Application definition
