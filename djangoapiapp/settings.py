042e0ba504515fc4dfe3810a72b6dfafb3429b48
bruce619 <chimuanyaibecheozor@gmail.com>
2020-09-24 01:46:12 +0100

    initial commit

--- djangoapiapp/settings.py
+++ djangoapiapp/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djangoapiapp.urls'
@@ -165,7 +166,9 @@ STATICFILES_DIRS = [
 # Media directory
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # A bootstrap 4 form
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
